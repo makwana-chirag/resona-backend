9c753096f055efe6cb64a0b6c378aedf2a293344
Chirag Makwana <am.chirag.pm@gmail.com>
2026-04-01 14:36:09 +0530

    swagger option udpated

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import { Application, Request, Response } from "express";
 import express from "express";
 import cors from "cors";
 import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from 'swagger-ui-express';
 
 const app:Application = express();
 
@@ -11,24 +12,26 @@ app.use(express.json())
 
 // Swagger Configuration
 const swaggerOptions = {
+    definition: {
     openapi : "3.0.0",
     info:{
         title : "Resona API",
         version : "1.0.0",
         description : "Music tracking application backend"
     },
-    server : [
+    servers : [
         {
-            url : "http://localhost:3000"
+            url : "http://localhost:3001"
         }
     ],
+    },
     apis:[
-        "./src/routers/*.ts"
+        "./src/routes/*.ts"
     ]
 };
 
 const swaggerSpec = swaggerJsdoc(swaggerOptions)
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 // Basic health check 
 app.get("/",(req:Request, res:Response)=>{
     res.json({
