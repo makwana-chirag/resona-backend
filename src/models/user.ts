4072a796d3b67da77cae57617ea544afa10d4fd9
Chirag Makwana <am.chirag.pm@gmail.com>
2026-04-07 12:29:18 +0530

    user route created

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,9 +1,25 @@
+import { body } from "express-validator";
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
+export const userSchema = new mongoose.Schema({
     name : {type : String, required : true, trim : true , minlength : 3, maxlength : 50},
     email : {type : String, required : true, unique : true, trim : true},
-    password : {type : String, required : true, trim : true, minlength : 8 , maxlength : 8},
+    password : {type : String, required : true, trim : true, minlength : 8 , maxlength : 128},
 })
 
 export const User = mongoose.model('User', userSchema) 
+
+export interface UserData {
+    name : string,
+    email : string,
+    password : string,
+}
+
+export const userValidation = (userData: UserData) =>{
+    const schema = [
+        body('name').isString().isLength({min:3,max:50}),
+        body('email').isEmail(),
+        body('password').isLength({min:8,max:128})
+    ]
+    return schema;
+}
